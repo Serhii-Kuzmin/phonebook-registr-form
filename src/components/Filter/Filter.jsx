04f27404f5babb1d4b15c30305b0006d12e1da95
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-27 19:44:06 +0100

    update-filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,14 @@
-import { LabelStyle, InputStyle } from 'components/App.styled';
+import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { filterSet, getFilter } from '../../redux/filterSlice';
+import { filterSet, selectFilter } from 'redux/filterSlice';
+import { Avatar, TextField, Box } from '@mui/material';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import { avatarStyle } from 'pages/StylePages';
+import { boxFilterStyle } from './StyleFilter';
 
 export const Filter = () => {
   const dispatch = useDispatch();
-  const filterPhoneBook = useSelector(getFilter);
+  const filterPhoneBook = useSelector(selectFilter);
 
   const onChangeFilter = event => {
     const { value } = event.currentTarget;
@@ -12,16 +16,28 @@ export const Filter = () => {
   };
 
   return (
-    <LabelStyle>
-      Find contacts by name:
-      <InputStyle
+    <Box component="div" sx={boxFilterStyle}>
+      <Avatar sx={avatarStyle}>
+        <PersonSearchIcon />
+      </Avatar>
+      <TextField
+        margin="normal"
+        inputProps={{
+          inputMode: 'text',
+          pattern: '^[a-zA-Zа-яА-Я]+(([a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$',
+        }}
+        sx={{
+          width: 324,
+          bgcolor: 'rgba(208, 224, 241, 0.822)',
+        }}
+        label="Find contacts by name:"
         type="text"
         name="filter"
         value={filterPhoneBook}
         title="Enter the name"
-        required
         onChange={onChangeFilter}
+        // pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
       />
-    </LabelStyle>
+    </Box>
   );
 };
