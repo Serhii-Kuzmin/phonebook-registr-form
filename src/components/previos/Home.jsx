d1eeb4669525f618383456bb554e0053c302ac84
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-27 23:53:12 +0100

    home

---
+++ src/components/previos/Home.jsx
@@ -0,0 +1,11 @@
+import { MainTitle } from 'components/Layout/Layout.styled';
+
+const Home = () => {
+  return (
+    <>
+      <MainTitle>PhoneBook</MainTitle>
+    </>
+  );
+};
+
+export default Home;
