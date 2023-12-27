6891d75a5c44b9422a7b8aa6e7b644ad35a7f8ce
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-27 20:09:06 +0100

    update-form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { useDispatch, useSelector } from 'react-redux';
-import { getPhoneBookValue } from '../../redux/phoneBook/phoneBookSlice';
-import { FormStyle } from './Form.styled';
-import { InputStyle, LabelStyle, ButtonStyle } from 'components/App.styled';
-import { postContactThunk } from '../../redux/services/fetchContacts';
+import {
+  selectIsContactAdd,
+  selectPhoneBookValue,
+} from 'redux/phoneBook/phoneSelector';
+import { postContactThunk } from 'services/fetchContacts';
+import { Avatar, Button, TextField, Box, Typography } from '@mui/material';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import { LoadAdd } from 'components/Loader/Loader';
+import { avatarStyle } from 'pages/StylePages';
 
 export const options = {
   width: '400px',
   position: 'center-center',
-  timeout: 3000,
+  timeout: 1500,
   fontSize: '20px',
 };
 
 export const Form = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const [add, setAdd] = useState(false);
+
   const dispatch = useDispatch();
-  const phoneBook = useSelector(getPhoneBookValue);
+  const phoneBook = useSelector(selectPhoneBookValue);
+  const isContactAdd = useSelector(selectIsContactAdd);
+
+  useEffect(() => {
+    setAdd(false);
+  }, [phoneBook]);
+
+  useEffect(() => {
+    if (isContactAdd) {
+      reset();
+    }
+  }, [isContactAdd]);
 
   const onSubmitAddContact = event => {
     event.preventDefault();
@@ -27,10 +46,8 @@ export const Form = () => {
       Notify.warning(`${newObj.name} is already in contacts`, options);
       return;
     }
-
+    setAdd(true);
     dispatch(postContactThunk(newObj));
-
-    reset();
   };
 
   const isNameNew = (phoneBook, newObj) => {
@@ -60,28 +77,55 @@ export const Form = () => {
   };
 
   return (
-    <FormStyle onSubmit={onSubmitAddContact}>
-      <LabelStyle>
-        Name:
-        <InputStyle
+    <>
+      <Avatar sx={avatarStyle}>
+        <ContactsIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Add Contact
+      </Typography>
+      <Box component="form" onSubmit={onSubmitAddContact} sx={{ mt: 1 }}>
+        <TextField
+          sx={{ backgroundColor: 'rgba(208, 224, 241, 0.822)' }}
+          // Validation
+          inputProps={{
+            inputMode: 'text',
+            pattern: '^[a-zA-Zа-яА-Я]+(([a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$',
+          }}
+          margin="normal"
+          fullWidth
+          label="Name"
           type="text"
           name="name"
           value={name}
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={onChangeInput}
         />
-      </LabelStyle>
-      <LabelStyle>
-        Phone number:
-        <InputStyle
+        <TextField
+          // Validation
+          sx={{ backgroundColor: 'rgba(208, 224, 241, 0.822)' }}
+          inputProps={{ inputMode: 'tel', pattern: '[0-9]*' }}
+          margin="normal"
+          fullWidth
+          label="Phone number"
           type="tel"
           name="number"
           value={number}
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={onChangeInput}
         />
-      </LabelStyle>
-      <ButtonStyle type="submit">Add contact</ButtonStyle>
-    </FormStyle>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2, display: 'flex', gap: 3 }}
+        >
+          {add && <LoadAdd />}
+          <p>Add contact</p>
+        </Button>
+      </Box>
+    </>
   );
 };
--- src/components/Form/Form.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FormStyle = styled.form`
-  display: flex;
-  width: 400px;
-  padding: 10px;
-  gap: 20px;
-  flex-wrap: wrap;
-`;
